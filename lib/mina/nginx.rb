e2abe7ffa1efa59941462ce7417ca3083dd4175b
Odin Dutton <odindutton@gmail.com>
2016-10-24 17:11:16 +1100

    Update for mina >= 1

--- lib/mina/nginx.rb
+++ lib/mina/nginx.rb
@@ -1,38 +1,38 @@
 require "mina/nginx/version"
 
 namespace :nginx do
-  set_default :nginx_user,     "www-data"
-  set_default :nginx_group,    "www-data"
-  set_default :nginx_path,     "/etc/nginx"
-  set_default :nginx_config,   -> { "#{deploy_to}/#{shared_path}/config/nginx.conf" }
-  set_default :nginx_config_e, -> { "#{nginx_path}/sites-enabled/#{application}.conf" }
+  set :nginx_user,     "www-data"
+  set :nginx_group,    "www-data"
+  set :nginx_path,     "/etc/nginx"
+  set :nginx_config,   -> { "#{fetch(:shared_path)}/config/nginx.conf" }
+  set :nginx_config_e, -> { "#{fetch(:nginx_path)}/sites-enabled/#{application}.conf" }
 
   desc 'Setup Nginx'
   task :setup => :environment do
-    queue  %(echo "-----> Setup the nginx")
-    queue! %(touch #{nginx_config})
-    queue  %(echo "-----> Be sure to edit 'shared/config/nginx.conf'.")
+    command  %(Setup the nginx)
+    command %(touch #{fetch(:nginx_config)})
+    comment  %(Be sure to edit 'shared/config/nginx.conf'.)
   end
 
   desc 'Symlinking nginx config file'
   task :link => :environment do
-    queue  %(echo "-----> Symlinking nginx config file")
-    queue! %(sudo ln -nfs "#{nginx_config}" "#{nginx_config_e}")
+    comment  %(Symlinking nginx config file)
+    command %(sudo ln -nfs "#{fetch(:nginx_config)}" "#{fetch(:nginx_config_e)}")
   end
 
   desc 'Parse nginx configuration file and upload it to the server.'
   task :parse => :environment do
     content = erb(nginx_template)
-    queue %(echo '#{content}' > #{nginx_config})
-    queue %(cat #{nginx_config})
-    queue %(echo "-----> Be sure to edit 'shared/config/nginx.conf'.")
+    command %(echo '#{content}' > #{fetch(:nginx_config)})
+    command %(cat #{fetch(:nginx_config)})
+    comment %(Be sure to edit 'shared/config/nginx.conf'.)
   end
 
   %w(stop start restart reload status).each do |action|
     desc "#{action.capitalize} Nginx"
     task action.to_sym => :environment do
-      queue  %(echo "-----> #{action.capitalize} Nginx")
-      queue! "sudo service nginx #{action}"
+      comment  %(#{action.capitalize} Nginx)
+      command "sudo service nginx #{action}"
     end
   end
 
