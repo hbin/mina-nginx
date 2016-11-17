5f327d3f3df1fc099792ad091c5d0995179cba7b
Huang Bin <huangbin88@foxmail.com>
2016-11-17 16:23:26 +0800

    satisfy rubocop

--- lib/mina/nginx.rb
+++ lib/mina/nginx.rb
@@ -1,22 +1,22 @@
-require "mina/nginx/version"
+require 'mina/nginx/version'
 
 namespace :nginx do
-  set :nginx_user,     "www-data"
-  set :nginx_group,    "www-data"
-  set :nginx_path,     "/etc/nginx"
+  set :nginx_user,     'www-data'
+  set :nginx_group,    'www-data'
+  set :nginx_path,     '/etc/nginx'
   set :nginx_config,   -> { "#{fetch(:shared_path)}/config/nginx.conf" }
   set :nginx_config_e, -> { "#{fetch(:nginx_path)}/sites-enabled/#{application}.conf" }
 
   desc 'Setup Nginx'
   task :setup => :environment do
-    command  %(Setup the nginx)
+    command %(Setup the nginx)
     command %(touch #{fetch(:nginx_config)})
-    comment  %(Be sure to edit 'shared/config/nginx.conf'.)
+    comment %(Be sure to edit 'shared/config/nginx.conf'.)
   end
 
   desc 'Symlinking nginx config file'
   task :link => :environment do
-    comment  %(Symlinking nginx config file)
+    comment %(Symlinking nginx config file)
     command %(sudo ln -nfs "#{fetch(:nginx_config)}" "#{fetch(:nginx_config_e)}")
   end
 
@@ -31,14 +31,14 @@ namespace :nginx do
   %w(stop start restart reload status).each do |action|
     desc "#{action.capitalize} Nginx"
     task action.to_sym => :environment do
-      comment  %(#{action.capitalize} Nginx)
+      comment %(#{action.capitalize} Nginx)
       command "sudo service nginx #{action}"
     end
   end
 
   private
 
-    def nginx_template
-      File.expand_path('../templates/nginx.conf.erb', __FILE__)
-    end
+  def nginx_template
+    File.expand_path('../templates/nginx.conf.erb', __FILE__)
+  end
 end
--- mina-nginx.gemspec
+++ mina-nginx.gemspec
@@ -4,22 +4,22 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'mina/nginx/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "mina-nginx"
+  spec.name          = 'mina-nginx'
   spec.version       = Mina::Nginx::VERSION
-  spec.authors       = ["hbin"]
-  spec.email         = ["huangbin88@foxmail.com"]
-  spec.summary       = %q{Mina tasks for handle with Nginx.}
-  spec.description   = %q{Configuration and managements Mina tasks for Nginx.}
-  spec.homepage      = "https://github.com/hbin/mina-nginx.git"
-  spec.license       = "MIT"
+  spec.authors       = ['hbin']
+  spec.email         = ['huangbin88@foxmail.com']
+  spec.summary       = %(Mina tasks for handle with Nginx.)
+  spec.description   = %(Configuration and managements Mina tasks for Nginx.)
+  spec.homepage      = 'https://github.com/hbin/mina-nginx.git'
+  spec.license       = 'MIT'
 
-  spec.files         = `git ls-files`.split($/)
+  spec.files         = `git ls-files`.split($RS)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_dependency "mina", "~> 1"
+  spec.add_dependency 'mina', '~> 1'
 
-  spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency 'bundler', '~> 1.5'
+  spec.add_development_dependency 'rake', '~> 0'
 end
